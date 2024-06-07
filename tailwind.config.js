0282616dfb74f911c650b3e3b060621314d1ad54
Tanveer Hossain Jony <thjbd19@gmail.com>
2024-06-07 23:29:13 +0600

    form work in process

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,11 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx,css}"],
   theme: {
+    colors: {
+      "primary-color": "#059669",
+      "secondary-color": "#f97316",
+      "custom-color-1": "#D3E0DC",
+    },
     extend: {},
   },
   plugins: [require("daisyui")],
